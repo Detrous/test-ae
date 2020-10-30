93756661c0f0892f3e005d0c0fed418497d95750
Artem Slobodian <artem.slobodian@externals.adidas.com>
2020-10-30 12:44:50 +0200

    Search endpoint

--- src/photo_api/routes.py
+++ src/photo_api/routes.py
@@ -1,3 +1,13 @@
-from flask import jsonify
+from flask import Response, jsonify
 
-from . import app
+from . import CACHE, CONFIG, app
+from .models import Photo
+
+
+@app.route("/search/<search_term>")
+@CACHE.memoize(CONFIG.get("api.search_cache_time"))
+def index(search_term):
+    photos = Photo.objects.search_text(search_term)
+    if photos.count() != 0:
+        return Response(photos.to_json(), content_type="application/json")
+    return Response(status=404)
